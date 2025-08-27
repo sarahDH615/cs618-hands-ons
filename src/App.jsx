9f45e60549d1fbcbcd845fa5ca7e93560f615b58
Sarah DH <72268326+sarahDH615@users.noreply.github.com>
2025-08-27 00:02:31 +0000

    refactor: use TanStack to wrap App

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,13 @@
-import { PostList } from './components/PostList.jsx'
-import { CreatePost } from './components/CreatePost.jsx'
-import { PostFilter } from './components/PostFilter.jsx'
-import { PostSorting } from './components/PostSorting.jsx'
-const posts = [
-  {
-    title: 'Full-Stack React Projects',
-    contents: "Let's become full-stack developers!",
-    author: 'Daniel Bugl',
-  },
-  { title: 'Hello React!', contents: 'New article', author: 'S. Hood' },
-]
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Blog } from './Blog.jsx'
+
+// client to call the backend
+const queryClient = new QueryClient()
+
 export function App() {
   return (
-    <div style={{ padding: 8 }}>
-      <CreatePost />
-      <br />
-      <br />
-      Filter by:
-      <PostFilter field='author' />
-      <br />
-      <PostSorting fields={['createdAt', 'updatedAt']} />
-      <hr />
-      <PostList posts={posts} />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Blog />
+    </QueryClientProvider>
   )
 }
