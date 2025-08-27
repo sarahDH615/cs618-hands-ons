da707e3d8bd41738c6a64dee54c2e38d3c0a5d1c
Sarah DH <72268326+sarahDH615@users.noreply.github.com>
2025-08-27 00:24:14 +0000

    feat: connect frontend and backend

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -2,15 +2,17 @@ import { PostList } from './components/PostList.jsx'
 import { CreatePost } from './components/CreatePost.jsx'
 import { PostFilter } from './components/PostFilter.jsx'
 import { PostSorting } from './components/PostSorting.jsx'
-const posts = [
-  {
-    title: 'Full-Stack React Projects',
-    contents: "Let's become full-stack developers!",
-    author: 'Daniel Bugl',
-  },
-  { title: 'Hello React!', contents: 'New article', author: 'S. Hood' },
-]
+import { useQuery } from '@tanstack/react-query'
+import { getPosts } from './api/posts.js'
+
 export function Blog() {
+  // create query to call the backend and read an endpoint
+  const postsQuery = useQuery({
+    queryKey: ['posts'], // the endpoint it reads
+    queryFn: () => getPosts(), // the function it calls to read the endpoint
+  })
+
+  const posts = postsQuery.data ?? [] // get data from the query
   return (
     <div style={{ padding: 8 }}>
       <CreatePost />
