5afec19fe86e2fe9e0abc23cfad709601ce8c14b
Sarah DH <72268326+sarahDH615@users.noreply.github.com>
2025-08-27 23:08:33 +0000

    feat: createPost functionality from the frontend

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,21 +1,67 @@
+import { useState } from 'react'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { createPost } from '../api/posts'
+
 export function CreatePost() {
+  const [title, setTitle] = useState('') // default: ''
+  const [author, setAuthor] = useState('')
+  const [contents, setContents] = useState('')
+
+  const queryClient = useQueryClient()
+  const createPostMutation = useMutation({
+    mutationFn: () => createPost({ title, author, contents }),
+    onSuccess: () => queryClient.invalidateQueries(['posts']), // means only the posts part of the page will update
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    createPostMutation.mutate()
+  }
+
   //   e.preventDefault prevents page refresh when a form is submitted
+  // prevent the submit button from clicking when there's no title or a post is pending
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
+    <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor='create-title'>Title: </label>
-        <input type='text' name='create-title' id='create-title' />
+        <input
+          type='text'
+          name='create-title'
+          id='create-title'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
       </div>
       <br />
       <div>
         <label htmlFor='create-author'>Author: </label>
-        <input type='text' name='create-author' id='create-author' />
+        <input
+          type='text'
+          name='create-author'
+          id='create-author'
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
       </div>
       <br />
-      <textarea />
+      <textarea
+        value={contents}
+        onChange={(e) => setContents(e.target.value)}
+      />
       <br />
       <br />
-      <input type='submit' value='Create' />
+      <input
+        type='submit'
+        value={createPostMutation.isPending ? 'Creating...' : 'Create'}
+        disabled={!title || createPostMutation.isPending}
+      />
+
+      {createPostMutation.isSuccess ? (
+        <>
+          <br />
+          Post successfully created!
+        </>
+      ) : null}
     </form>
   )
 }
