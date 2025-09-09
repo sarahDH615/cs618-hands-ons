044cfee5877364a533b193b5bfbfcfda6abb1a20
Sarah DH <72268326+sarahDH615@users.noreply.github.com>
2025-09-09 23:38:50 +0000

    fix: attempt 3

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -20,7 +20,7 @@ export function Blog() {
   const posts = postsQuery.data ?? [] // get data from the query
   return (
     <div style={{ padding: 8 }}>
-      <h1>This is my blog</h1>
+      <h1>Hi. This is my blog</h1>
       <CreatePost />
       <br />
       <br />
